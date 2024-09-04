1b6f3a25e856e150ffbc06b6cc80061423fcc669
mitrikhoury <mitkhoury@gmail.com>
2024-09-04 15:37:30 +0300

    creating button birthday

--- React/Class Components/prop_task/src/component/PersonCard.js
+++ React/Class Components/prop_task/src/component/PersonCard.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PersonCard = (props) => <div>
-    <h1>{props.firstName} , {props.lastName}</h1>
-    <p>{props.age}</p>
-    <p>Hair Color :{props.hairColor}</p>         
-    </div>
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    incrementAge = () => {
+        this.setState({ age: parseInt(this.state.age) + 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.firstName} , {this.props.lastName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.incrementAge}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+            </div>
+        );
+    }
+}
 
 export default PersonCard;
+
+
+// const PersonCard = (props) => <div>
+//     <h1>{props.firstName} , {props.lastName}</h1>
+//     <p>{props.age}</p>
+//     <p>Hair Color :{props.hairColor}</p>    
+//     <button></button>     
+//     </div>
+
+
